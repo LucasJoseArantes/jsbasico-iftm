52b657485799ca544b02c7a1f54bd7de7f88d035
Lucas Jose <lucas.arnts@outlook.com>
2023-09-11 18:25:04 -0300

    Update script03.js

--- Listas/lista04/ex03/script03.js
+++ Listas/lista04/ex03/script03.js
@@ -1,10 +1,9 @@
-var num = prompt("Informe um valor para N:");
-while (isNaN(numInt) || numInt <= 0) {
+var num = parseInt(prompt("Informe um valor para N:"));
+
+while (isNaN(num) || num <= 0) {
     var num = prompt("Por favor, insira um valor válido para N maior que 0.");
-} 
-var numInt = parseInt(num);
+}
 
-
-var ramNum = Math.floor(Math.random() * numInt) + 1;
-document.write("<h2>Número Aleatório entre 1 e " + numInt + ": " + ramNum + "</h2>");
+var ramNum = Math.floor(Math.random() * num) + 1;
+document.write("<h2>Número Aleatório entre 1 e " + num + ": " + ramNum + "</h2>");
     
