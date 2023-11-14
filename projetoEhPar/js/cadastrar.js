cead8f887c6a099af4c6876369fe85a5f1b7b54b
Lucas Jose <lucas.arnts@outlook.com>
2023-11-13 21:37:03 -0300

    Create Cadastro 'Even?'

---
+++ projetoEhPar/js/cadastrar.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", function (){
+    document.getElementById("btnCadastrar").addEventListener("click", cadastrarUsuario);
+
+    function cadastrarUsuario(){
+        var user = document.getElementById("txtUser").value;
+
+        var pwd = document.getElementById("txtPwd").value;
+
+        var checkPwd = document.getElementById("txtCheckPwd").value
+
+        if( user == "" || pwd == "" || checkPwd == ""){
+            alertWifi("Preencha todas as informações", false, 0, "", 30, "");
+        }
+
+        else{
+            
+            if( pwd == checkPwd){
+                var newUser = {nome:user , senha:pwd };
+                var vetUsers = localStorage.getItem("vetUsers");
+                
+                if(!vetUsers){
+                    // caso seja o primeiro cadastro = o vetor ainda não existe 
+                    var vet = [];
+                    vet.push(newUser);
+                    localStorage.setItem("vetUsers", JSON.stringify(vet));
+                // CRIAÇÃO DE CHAVE, NOME vetUsers E VALOR vet (VETOR QUE ARMAZENA NOVOS USUÁRIOS) CONVERTIDO EM STRING
+                }
+                else{
+                    // O VETOR vet VEM COMO UM JSON DO LOCALSTORAGE, ENTÃO PRECISAMOS CONVERTER PARA ADICIONAR OS NOVOS USUÁRIOS
+                    vet = JSON.parse(vetUsers);
+                    vet.push(newUser);
+
+                    localStorage.setItem("vetUsers", JSON.stringify(vet));
+                }
+                
+                alertWifi("Usuário cadastrado com sucesso", false, 0, "", 30, "");                
+          
+             }
+             
+        }
+
+    }
+
+
+});
