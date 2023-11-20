0c730b2ab508ec8fd8383753ce74e495ec0e7dcc
Lucas Jose <lucas.arnts@outlook.com>
2023-11-20 17:22:36 -0300

    Update Login
    Corrigindo detalhes de verificação

--- projetoEhPar/js/index.js
+++ projetoEhPar/js/index.js
@@ -17,6 +17,23 @@ window.addEventListener("load", function (){
                 alertWifi("Ainda não há usuários Cadastrados", false, 0, "", 30, "");
             }
 
+            else{
+                vetUsers = JSON.parse(vetUsers);
+                var findUser = false;
+
+                for(i=0; i < vetUsers.length; i++) {
+                    
+                    if(vetUsers[i].nome == user && vetUsers[i].senha == pwd){
+                        findUser = true;
+                        break;
+                    }
+                   
+                }
+
+                if(findUser == true) alertWifi("Usuário encontrado", false, 0, "", 30, "jogo.html");
+                else alertWifi("Usuário não encontrado", false, 0, "", 30, "");
+            }
+
         }
 
     }
